5a714f59231cbd1eba403940d7b265afba5466ad
Ribot1 <el.ribot@gmail.com>
2022-04-16 15:19:55 -0400

    creacion de templates y footer

--- punto_verde/aapp/urls.py
+++ punto_verde/aapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from.views import home, registro
+from.views import  estado, home, llenado, recicla, registro
 
 urlpatterns = [
     path('', home, name="home"),
     path('registro/', registro, name="registro"),
+    path('estado/', estado, name="estado"),
+    path('recicla/', recicla, name="recicla"),
+    path('llenado/', llenado, name="llenado"),
 
 
 ]
--- punto_verde/aapp/views.py
+++ punto_verde/aapp/views.py
@@ -9,3 +9,12 @@ def home(request):
 
 def registro(request):
    return render(request, 'app/registro.html')
+
+def estado(request):
+   return render(request, 'app/estado.html')
+
+def recicla(request):
+   return render(request, 'app/recicla.html')
+
+def llenado(request):
+   return render(request, 'app/llenado.html')
